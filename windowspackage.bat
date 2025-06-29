7b2b801f0172ee2ef954ff4738b1f73016510b55
Lorenzo <farrancelorenzo@gmail.com>
2025-06-29 11:47:04 +1000

    Update windowspackage.bat

--- windowspackage.bat
+++ windowspackage.bat
@@ -1,5 +1,5 @@
 @echo off
-powershell open https://raw.githubusercontent.com/detectivesheepy/detectivepi/refs/heads/main/installing.txt
-powershell git clone https://github.com/Botspot/pi-apps.git
-powershell git clone https://github.com/GNOME/gimp.git
-powershell open https://raw.githubusercontent.com/detectivesheepy/detectivepi/refs/heads/main/installed.txt
+open https://raw.githubusercontent.com/detectivesheepy/detectivepi/refs/heads/main/installing.txt
+git clone https://github.com/Botspot/pi-apps.git
+git clone https://github.com/GNOME/gimp.git
+open https://raw.githubusercontent.com/detectivesheepy/detectivepi/refs/heads/main/installed.txt
